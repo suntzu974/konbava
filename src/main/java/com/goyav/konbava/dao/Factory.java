95af0493d26f550a399f6c973bbc85b08b7e212f
Jeannick GRONDIN <jeannick.grondin@gmail.com>
2021-12-14 21:58:56 +0400

    tes

--- src/main/java/com/goyav/konbava/dao/Factory.java
+++ src/main/java/com/goyav/konbava/dao/Factory.java
@@ -10,33 +10,68 @@ public class Factory {
 	private static String user;
 	private static String password;
 
-	public Factory(String url,String user, String password) {
+	public Factory(String url) {
 		super();
 		this.url = url;
-		this.user = user;
-		this.password = password;
 	}
+
+	public Factory(String url,String user, String password) {
+		super();
+		Factory.url = url;
+		Factory.user = user;
+		Factory.password = password;
+	}
+	
+	public static String getUrl() {
+		return url;
+	}
+
+	public static void setUrl(String url) {
+		Factory.url = url;
+	}
+
+	public static String getUser() {
+		return user;
+	}
+
+	public static void setUser(String user) {
+		Factory.user = user;
+	}
+
+	public static String getPassword() {
+		return password;
+	}
+
+	public static void setPassword(String password) {
+		Factory.password = password;
+	}
+
 	public static Factory getInstance() {
 		try {
 			Class.forName("org.postgresql.Driver");
 			} catch (ClassNotFoundException e) {
 			
 		}
+/*		String host = System.getenv("JDBC_POSTGRES_URL");
+		String user = System.getenv("PG_USER");
+		String password = System.getenv("PG_PASSWORD");*/
+
+//		Factory instance = new Factory (host/*,user,password*/);
+		Factory instance = new Factory ("jdbc:postgresql://localhost:5432/moinlela?user=suntzu974&password=HpNKUsNN27031968&ssl=true;");
+		return instance;
+	}
+	public static Connection getConnection() throws SQLException  {
+/*		Properties props = new Properties();
 		String host = System.getenv("JDBC_POSTGRES_URL");
 		String user = System.getenv("PG_USER");
 		String password = System.getenv("PG_PASSWORD");
 
-		Factory instance = new Factory (host,user,password);
-		return instance;
-	}
-	public static Connection getConnection() throws SQLException  {
-		Properties props = new Properties();
 		
 		props.setProperty("user",user);
 		props.setProperty("password",password);
-		props.setProperty("ssl","true");
+		props.setProperty("ssl","true");*/
 
-			return DriverManager.getConnection(url,props);
+			return DriverManager.getConnection(url);
 	}
 	public CustomerDao getCustomerDao() {
 		return new CustomerDaoImpl(this);
