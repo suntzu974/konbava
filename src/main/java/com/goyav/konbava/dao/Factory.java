339893c470d5f4e1140c7f36704f5827251c154e
Jeannick GRONDIN <jeannick.grondin@gmail.com>
2021-12-14 22:50:52 +0400

    change

--- src/main/java/com/goyav/konbava/dao/Factory.java
+++ src/main/java/com/goyav/konbava/dao/Factory.java
@@ -7,21 +7,12 @@ import java.util.Properties;
 
 public class Factory {
 	private static String url;
-	private static String user;
-	private static String password;
 
 	public Factory(String url) {
 		super();
 		this.url = url;
 	}
 
-	public Factory(String url,String user, String password) {
-		super();
-		Factory.url = url;
-		Factory.user = user;
-		Factory.password = password;
-	}
-	
 	public static String getUrl() {
 		return url;
 	}
@@ -30,48 +21,19 @@ public class Factory {
 		Factory.url = url;
 	}
 
-	public static String getUser() {
-		return user;
-	}
-
-	public static void setUser(String user) {
-		Factory.user = user;
-	}
-
-	public static String getPassword() {
-		return password;
-	}
-
-	public static void setPassword(String password) {
-		Factory.password = password;
-	}
-
+	
 	public static Factory getInstance() {
 		try {
 			Class.forName("org.postgresql.Driver");
 			} catch (ClassNotFoundException e) {
 			
 		}
-/*		String host = System.getenv("JDBC_POSTGRES_URL");
-		String user = System.getenv("PG_USER");
-		String password = System.getenv("PG_PASSWORD");*/
 
-//		Factory instance = new Factory (host/*,user,password*/);
 		Factory instance = new Factory ("jdbc:postgresql://localhost:5432/moinlela?user=suntzu974&password=HpNKUsNN27031968&ssl=true;");
 		return instance;
 	}
 	public static Connection getConnection() throws SQLException  {
-/*		Properties props = new Properties();
-		String host = System.getenv("JDBC_POSTGRES_URL");
-		String user = System.getenv("PG_USER");
-		String password = System.getenv("PG_PASSWORD");
-
-		
-		props.setProperty("user",user);
-		props.setProperty("password",password);
-		props.setProperty("ssl","true");*/
-
-			return DriverManager.getConnection(url);
+		return DriverManager.getConnection(url);
 	}
 	public CustomerDao getCustomerDao() {
 		return new CustomerDaoImpl(this);
